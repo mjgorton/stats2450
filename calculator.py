45ad3f682ecb3da0f756dc4ec9230c10983f8890
mjgorton <madelynn_gorton1@baylor.edu>
2024-11-18 14:19:39 -0700

    First file

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#Exercise 2
+
+def calculate(num1, num2):
+    total = num1 +num2
+    diff = num1 - num2
+    prod = num1 * num2
+    quot = num1 / num2
+
+    return total, diff, prod, quot
+
+print(calculate(10, 3))
+
+total, diff, prod, quot = calculate(10, 0)
+
